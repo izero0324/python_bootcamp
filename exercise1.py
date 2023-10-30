38ef01db4c169b3a22c3318f5c533f29958e9af4
Chia-Hung Yang (k23110833) <k23110833@nmslab-004115.nms.kcl.ac.uk>
2023-10-30 11:57:28 +0000

    exercise 1 and 2

---
+++ exercise1.py
@@ -0,0 +1,5 @@
+my_neighbours_name = 'rowan'
+my_name = 'andrew'
+print('Hi, ', my_neighbours_name, '! My name is ', my_name)
+my_age = 26
+print('My age is :', my_age)
---
+++ exercise2.py
@@ -0,0 +1,5 @@
+print("What's your favourite movie?")
+user_fav = input()
+
+my_fav = 'Back to Future'
+print("I like %s too, but my favourite is %s" %(user_fav, my_fav))
